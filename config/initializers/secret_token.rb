99d343b555f50f805dd48086857d70fb7c4e9880
Fabiana Anjos Campelo <fabyanjos@gmail.com>
2013-09-06 18:14:32 +0200

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BuyList::Application.config.secret_token = '1d0270fed4397d8d56432b8a496d695d78294c387d5de1e66564cd176297358517a1a88890d0d4714177f24347a49f85a32373cd6cf4a0355e7fb97c9d548135'
