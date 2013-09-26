0813d9164b8f2a624c99a93be43657375c80b8a0
Fabiana Anjos Campelo <fabyanjos@gmail.com>
2013-09-26 16:33:39 +0200

    environment variable file

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module BuyList
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.before.configuration do
+    config.before_configuration do
         env_file = File.join(Rails.root, 'config', 'local_env.yml')
         YAML.load(File.open(env_file)).each do |key, value|
             ENV[key.to_s] = value
