f8a058b44948c6fdf019606ebfc44376f1e09f74
Fabiana Anjos Campelo <fabyanjos@gmail.com>
2013-11-18 12:47:13 +0100

    add email to user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
+ 	before_filter :set_locale
  	protect_from_forgery
-	before_filter :set_locale
 
 	def set_locale
-		I18n.locale = params[:locale]
+		I18n.locale = params[:locale] if params[:locale]
 	end
 
 	private
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131114212103) do
+ActiveRecord::Schema.define(:version => 20131118113302) do
 
   create_table "products", :force => true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(:version => 20131114212103) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
+    t.string   "email"
   end
 
 end
